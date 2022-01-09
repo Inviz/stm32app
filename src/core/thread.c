5b4c2cf2b33fe941dbdc42e7ffe89ef5b02e23aa
Yaroslaff Fedin <imac@imacs-iMac.local>
2022-01-09 12:32:38 +0800

    Write comments

--- src/core/thread.c
+++ src/core/thread.c
@@ -105,6 +105,55 @@ size_t app_thread_get_tick_index(app_thread_t *thread) {
     return -1;
 }
 
+static void app_thread_event_dispatch(app_thread_t *thread, app_event_t *event, size_t tick_index, device_t *first_device);
+static void app_thread_event_defer(app_thread_t *thread, app_event_t *event, app_event_status_t previous_status, uint32_t *deferred_count);
+static void app_thread_event_ingest(app_thread_t *thread, app_event_t *event, size_t deferred_count);
+
+/* A generic RTOS task function that handles all typical configurations of threads. It supports following features or combinations of
+  them:
+  - Waking up devices on individual software timers to simulate delays and periodical work
+  - Aborting or speeding up the software timer
+  - Broadcasting events to multiple listeners
+  - Broadcasting events to first taker
+  - Deferring events for later handling if device was busy
+  - Maintaining queue of events OR lightweight event event mailbox slot.
+
+  An app has at least five of these threads with different priorities. Devices declare "ticks" which act as callbacks for a corresponding
+  thread. This way devices can prioritize parts of their logic to play well in shared environment. For example devices may afford doing
+  long-er processing of data in background without fearing of blocking networking or handling inputs for the whole app. Another benefit of
+  allowing device to run logic with different priorities is responsiveness and flexibility. An high-priority input event may tell device to
+  abort longer task and do something else instead.
+
+  A downside of multiple devices sharing the same thread, is that there is no time-slicing of work within a thread between devices.
+  Essentially, until a device finishes its work the whole thread (including processing of new events) is blocked, unless a thread with
+  higher priority tells the device to stop. So devices still need to be mindful of blocking the cpu. There can be a few solutions to this:
+  - Devices can split their workload into chunks.  Execution control would need to be yielded after executing each chunk by using software
+    timer to wake later to execute the next chunk. The downside to this is that each chunk can only be scheduled with resolution of a time
+  slice (1ms).
+  - Devices can also create their own custom threads with own queues, leveraging all the dispatching and queue-management mechanisms. In
+    this case RTOS will use time-slicing to periodically break up execution and allow other threads to work.
+ */
+void app_thread_execute(app_thread_t *thread) {
+    thread->last_time = xTaskGetTickCount();
+    app_t *app = thread->device->app;
+    size_t tick_index = app_thread_get_tick_index(thread);                                   // which device tick handles this thread
+    device_t *first_device = app_thread_filter_devices(thread);                              // linked list of devices declaring the tick
+    app_event_t event = {.producer = thread->device->app->device, .type = APP_EVENT_WAKEUP}; // incoming event from the queue
+    size_t deferred_count = 0;                                                               // Counter of re-queued events
+
+    if (first_device == NULL) {
+        log_printf("No devices subsribe to thread #%i\n", thread_index);
+        return;
+    }
+
+    while (true) {
+        app_event_status_t previous_event_status = event.status == APP_EVENT_DEFERRED;
+        app_thread_event_dispatch(thread, &event, first_device, tick_index);
+        app_thread_event_defer(thread, &event, previous_event_status, &deferred_count);
+        app_thread_event_ingest(thread, &event, deferred_count);
+    }
+}
+
 /* Notify all interested devices of a new event.
  * - Device may want to own the event exclusvely, in that case other devices will not receive it
  * - Device may be busy during processing other event, when the new event it wants to exclusively handle arrives. Event will be kept in a
@@ -220,37 +269,6 @@ static void app_thread_event_ingest(app_thread_t *thread, app_event_t *event, si
                 memcpy(event, (uint32_t *)notification, sizeof(app_event_t));
             }
         }
-    }
-}
-
-/* A generic RTOS task function that handles all typical configurations of threads. It supports following features or combinations of them:
-  - Waking up devices on individual software timers to simulate delays and periodical work
-  - Broadcasting events to multiple listeners
-  - Broadcasting events to first taker
-  - Deferring events for later handling if device was busy
-  - Maintaining queue of events OR lightweight event event mailbox slot.
-
-  An app has at least five of these threads with different priorities, so devices can declare their work with a specific priority. In
-  addition devices can also create their own custom threads with own queues, leveraging all the dispatching and queue-management mechanisms.
- */
-void app_thread_execute(app_thread_t *thread) {
-    thread->last_time = xTaskGetTickCount();
-    app_t *app = thread->device->app;
-    size_t tick_index = app_thread_get_tick_index(thread);                                   // which device tick handles this thread
-    device_t *first_device = app_thread_filter_devices(thread);                              // linked list of devices declaring the tick
-    app_event_t event = {.producer = thread->device->app->device, .type = APP_EVENT_WAKEUP}; // incoming event from the queue
-    size_t deferred_count = 0;                                                               // Counter of re-queued events
-
-    if (first_device == NULL) {
-        log_printf("No devices subsribe to thread #%i\n", thread_index);
-        return;
-    }
-
-    while (true) {
-        app_event_status_t previous_event_status = event.status == APP_EVENT_DEFERRED;
-        app_thread_event_dispatch(thread, &event, first_device, tick_index);
-        app_thread_event_defer(thread, &event, previous_event_status, &deferred_count);
-        app_thread_event_ingest(thread, &event, deferred_count);
     }
 }
 
